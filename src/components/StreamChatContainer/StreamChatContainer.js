f86d3d9caa2ae275b4af39b2740cd310ecae4ec3
mboraski <markb539@gmail.com>
2020-10-28 13:36:08 -0600

    custom message and attachments

--- src/components/StreamChatContainer/StreamChatContainer.js
+++ src/components/StreamChatContainer/StreamChatContainer.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Chat, Channel, ChannelHeader, Window } from 'stream-chat-react';
+import {
+    Chat,
+    Channel,
+    ChannelHeader,
+    Attachment,
+    Window,
+} from 'stream-chat-react';
 import {
     MessageList,
     MessageInput,
@@ -14,6 +20,40 @@ import 'stream-chat-react/dist/css/index.css';
 const chatClient = new StreamChat('ycyq43kqr2em');
 const userToken =
     'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoic3dlZXQtaGF6ZS03In0.pbPehrBTUzAdZGq5ERdN-xCV-P2cqI-2k71uHUiyLjo';
+
+class MyAttachmentComponent extends React.Component {
+    render() {
+        const { attachments } = this.props;
+        attachments.forEach((attachment) => {
+            if (attachment.type === 'product') {
+                return (
+                    <div className="product">
+                        Product:
+                        <a
+                            href={attachment.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                src={attachment.image}
+                                alt="img"
+                                height={'100px'}
+                            />
+                            <br />
+                            {attachment.name}
+                        </a>
+                    </div>
+                );
+            }
+        });
+        return (
+            <div>
+                testing
+                <Attachment {...this.props} />
+            </div>
+        );
+    }
+}
 
 chatClient.setUser(
     {
@@ -30,9 +70,27 @@ const channel = chatClient.channel('livestream', 'spacex', {
     name: 'SpaceX launch discussion',
 });
 
+const attachments = [
+    {
+        type: 'product',
+        name: 'iPhone',
+        url: 'https://goo.gl/ppFmcR',
+        image: 'https://goo.gle/J2gQpi',
+    },
+];
+
+channel.sendMessage({
+    text: 'Your selected product is out of stock',
+    attachments: attachments,
+});
+
 const StreamChatContainer = () => (
     <Chat client={chatClient} theme={'livestream dark'}>
-        <Channel channel={channel} message={MessageLivestream}>
+        <Channel
+            channel={channel}
+            Attachment={MyAttachmentComponent}
+            message={MessageLivestream}
+        >
             <Window hideOnThread>
                 <ChannelHeader live />
                 <MessageList />
